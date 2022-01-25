03bee0c6b09ab58c9f6d5ff9cda0d53acbbd6d97
cincottash <cincotta@udel.edu>
2022-01-25 12:27:25 -0500

    removed rover if

--- main.py
+++ main.py
@@ -27,54 +27,53 @@ def getRoverInfo():
 
 
 def getRoverPics(savePath, roverInfo):
+
 	for rover in roverInfo:
-		if rover.name == 'Spirit':
-		
-			print(f'Creating dir: {savePath}{rover.name}\n')
-			os.system(f'mkdir {savePath}{rover.name}')
 
-			#print('name: {}\ncameras:{}\n\n'.format(rover.name, rover.cameras))
-			currentSol = 0
+		print(f'Creating dir: {savePath}{rover.name}\n')
+		os.system(f'mkdir {savePath}{rover.name}')
 
-			while(currentSol <= rover.maxSol):
-				#create the dir
-				print(f'Creating dir: {savePath}{rover.name}/{currentSol}\n')
-				os.system(f'mkdir {savePath}{rover.name}/{currentSol}')
+		currentSol = 0
 
-				#download each image from each camera on each day
-				for camera in rover.cameras:
-					print(f'Creating dir: {savePath}{rover.name}/{currentSol}/{camera}\n')
-					os.system(f'mkdir {savePath}{rover.name}/{currentSol}/{camera}')
+		while(currentSol <= rover.maxSol):
+			#create the dir
+			print(f'Creating dir: {savePath}{rover.name}/{currentSol}\n')
+			os.system(f'mkdir {savePath}{rover.name}/{currentSol}')
 
-					response = requests.get(f'https://mars-photos.herokuapp.com/api/v1/rovers/{rover.name}/photos?sol={currentSol}&camera={camera}')
+			#download each image from each camera on each day
+			for camera in rover.cameras:
+				print(f'Creating dir: {savePath}{rover.name}/{currentSol}/{camera}\n')
+				os.system(f'mkdir {savePath}{rover.name}/{currentSol}/{camera}')
 
-					jsonData = response.json()
+				response = requests.get(f'https://mars-photos.herokuapp.com/api/v1/rovers/{rover.name}/photos?sol={currentSol}&camera={camera}')
 
-					#download all the pics taken from that camera on this sol and save it
-					cameraPhotoCount = 0
-					for element in jsonData['photos']:
-						
-						url = element['img_src']
+				jsonData = response.json()
 
-						cmd = f'curl -L -o {savePath}{rover.name}/{currentSol}/{camera}/{cameraPhotoCount} {url}'
-						print(f'{cmd}\n')
-						os.system(cmd)
-						cameraPhotoCount += 1
+				#download all the pics taken from that camera on this sol and save it
+				cameraPhotoCount = 0
+				for element in jsonData['photos']:
+					
+					url = element['img_src']
 
-					#not every camera is used each day, so after we finish a sol, delete empty camera folders
-					if cameraPhotoCount == 0:
-						print(f'Deleting empty camera folder: {savePath}{rover.name}/{currentSol}/{camera}\n')
-						os.system(f'rmdir {savePath}{rover.name}/{currentSol}/{camera}')
+					cmd = f'curl -L -o {savePath}{rover.name}/{currentSol}/{camera}/{cameraPhotoCount} {url}'
+					print(f'{cmd}\n')
+					os.system(cmd)
+					cameraPhotoCount += 1
 
-				#not every day has pictures taken, if there were no pics on a day, delete that day's folder
-				folderCount = sum([len(folder) for r, d, folder in os.walk(f'{savePath}{rover.name}/{currentSol}')])
-				
-				if folderCount == 0:
-					print(f'Deleting empty sol folder: {savePath}{rover.name}/{currentSol}\n')
-					os.system(f'rmdir {savePath}{rover.name}/{currentSol}')
-				
+				#not every camera is used each day, so after we finish a sol, delete empty camera folders
+				if cameraPhotoCount == 0:
+					print(f'Deleting empty camera folder: {savePath}{rover.name}/{currentSol}/{camera}\n')
+					os.system(f'rmdir {savePath}{rover.name}/{currentSol}/{camera}')
 
-				currentSol += 1
+			#not every day has pictures taken, if there were no pics on a day, delete that day's folder
+			folderCount = sum([len(folder) for r, d, folder in os.walk(f'{savePath}{rover.name}/{currentSol}')])
+			
+			if folderCount == 0:
+				print(f'Deleting empty sol folder: {savePath}{rover.name}/{currentSol}\n')
+				os.system(f'rmdir {savePath}{rover.name}/{currentSol}')
+			
+
+			currentSol += 1
 
 
 		
